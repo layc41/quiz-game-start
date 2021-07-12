2969ff87bd920e9975a71140d6cacfa52277fa88
layc41 <64671431+layc41@users.noreply.github.com>
2021-07-12 12:06:09 -0500

    finished quiz project

---
+++ data.py
@@ -0,0 +1,128 @@
+question_data = [
+    {
+        "category": "Entertainment: Books",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The book 1984 was published in 1949.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Animals",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A bear does NOT defecate during hibernation. ",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Japanese Anime & Manga",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Gosho Aoyama Made This Series: (Detective Conan / Case Closed!)",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The United States of America declared their "
+                    "independence from the British Empire on July 4th, 1776.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Music",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "The song &quot;Mystery Train&quot; was released by artist "
+                    "&quot;Little Junior&#039;s Blue Flames&quot; in 1953.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Brandon Routh plays the titular character in the movie &quot;John Wick&quot;.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Television",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Like his character in &quot;Parks and Recreation&quot;, Aziz Ansari was born in South Carolina.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Zero factorial is equal to zero. ",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Television",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "In &quot;Star Trek: The Next Generation&quot;, Data is the only android in existence.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Francis Bacon died from a fatal case of pneumonia while "
+                    "he was attempting to preserve meat by stuffing a chicken with snow.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "YouTube personality Jenna Marbles served as an executive "
+                    "producer of the film Maximum Ride (2016).",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Musicals & Theatres",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "The protagonist&#039;s names in &#039;Who&#039;s "
+                    "Afraid of Virginia Woolf&#039;, George and Martha, were "
+                    "derived from George Washington and his wife.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    }
+]
